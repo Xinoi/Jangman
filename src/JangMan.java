c60a9b25cf3b411558436ebcc33c9683469e48e5
xinoi <zaunrithj@gmail.com>
2023-12-24 22:36:18 +0100

    created structure

---
+++ src/JangMan.java
@@ -0,0 +1,45 @@
+public class JangMan {
+
+    private final int MAX_WRONG_GUESSES = 10;
+    private String used_characters;
+    private int wrong_guesses;
+    private String current_word;
+
+    public JangMan() {
+        //to do
+    }
+
+    public JangMan(String word) {
+        //To do
+    }
+
+    public boolean makeGuess(char guess) {
+        //to do
+        return false;
+    }
+
+    public int wrongTriesLeft() {
+        //to do
+        return 0;
+    }
+
+    public String solution() {
+        //to do
+        return null;
+    }
+
+    public String toString() {
+        //to do
+        return null;
+    }
+
+    public boolean isWon() {
+        //to do
+        return false;
+    }
+
+    public boolean isGameOver() {
+        //to do
+        return false;
+    }
+}
